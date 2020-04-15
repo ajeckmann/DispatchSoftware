a604903149f627327f94d7ed0f63e48602d05327
Ari <ari@Aris-MacBook-Pro.local>
2020-04-14 20:21:21 -0500

    additions to dashboard page/consolidations of units/erasing of commas at ends of lists

--- Models/Unit.cs
+++ Models/Unit.cs
@@ -3,38 +3,35 @@ using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
-namespace Dispatch.Models
-{
-    public class Unit
-    {
+namespace Dispatch.Models {
+    public class Unit {
         [Key]
-        public int UnitId{get;set;}
+        public int UnitId { get; set; }
 
-        
-        [Required(ErrorMessage="Must include Unit Designation")]
-        public string NumberType{get;set;}
-        [Required(ErrorMessage="Must include Unit Number")]
-        [Range(1,500)]
-        public int Number{get;set;}
+        [Required (ErrorMessage = "Must include Unit Designation")]
+        public string NumberType { get; set; }
 
-        [Required(ErrorMessage="Must include Unit Capacity")]
-        public string UnitType{get;set;}
+        [Required (ErrorMessage = "Must include Unit Number")]
+        [Range (1, 500)]
+        public int Number { get; set; }
 
-        public List<Assignment> personnel{get;set;}
-        public List<Dispatchh> calls{get;set;}
+        [Required (ErrorMessage = "Must include Unit Capacity")]
+        public string UnitType { get; set; }
 
-        public int UserId{get;set;}
+        //navigation property to refer to association between rescuer and unit
+        public List<Assignment> personnel { get; set; }
 
-        public User creator{get;set;}
+        //navigation property to refer to association between incident and unit (a dispatch)
+        public List<Dispatchh> calls { get; set; }
 
-        public DateTime CreatedAt {get;set;}=DateTime.Now;
-        public DateTime UpdatedAt{get;set;}=DateTime.Now;
+        public int UserId { get; set; }
 
-        
+        public User creator { get; set; }
 
-        public Boolean IsAvailable{get;set;}=true;
-        
-        
+        public DateTime CreatedAt { get; set; } = DateTime.Now;
+        public DateTime UpdatedAt { get; set; } = DateTime.Now;
+
+        public Boolean IsAvailable { get; set; } = true;
 
     }
 }
